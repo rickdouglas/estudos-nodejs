e1e593fa82e51675e42f5a0e47f4def867224f87
rickdouglas <rickmiranda23@gmail.com>
2020-01-30 09:30:34 -0300

    Instalaçao do mongoose e inicio da criaçao de models

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express'); // importa o express
-const app = express();
+const mongoose = require('mongoose');// importa o mongoose
 
+
+const app = express(); // Inicia o app
+
+//Inicia o DB
+mongoose.connect('mongodb://localhost:27017/nodepai',
+ {useNewUrlParser: true});
+
+// Crinando a primeira rota
 app.get('/', (rec, res)=>{
 	res.send('Hello from the other side motherfucker !!!');
 });
