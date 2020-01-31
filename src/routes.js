8e3de901d2140691b849887412c56ec05bad8cb9
rickdouglas <rickmiranda23@gmail.com>
2020-01-30 23:21:26 -0300

    CRUD implementado e funcionando

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,10 @@ const ProductController = require("./controllers/ProductController");
 
 // Crinando a primeira rota
 routes.get("/products", ProductController.index);
+routes.get("/products/:id", ProductController.show);
 routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete("/products/:id", ProductController.destroy);
 
 
 module.exports = routes;
